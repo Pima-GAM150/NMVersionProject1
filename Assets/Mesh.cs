f50e8a8ac45102f7b92fc9901c9cc5286298edcb
Nate Mendoza <nmendoza82rg@aol.com>
2018-09-06 19:24:40 -0700

    fixx

---
+++ Assets/Mesh.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mesh : MonoBehaviour {
+
+
+    //trying to create a mesh thru code
+    // to build need to assign verts
+
+    public MeshFilter filter;
+
+    public Vector3[] verts;
+    public int[] tris;
+
+
+
+	void Start () {
+        Mesh myNewMesh = new Mesh();
+        myNewMesh.vertices = verts;
+        myNewMesh.triangles = tris;
+
+        filter.mesh = myNewMesh;
+    }
+	
+	
+	void Update () {
+		
+	}
+}
